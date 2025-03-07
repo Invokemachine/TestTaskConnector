77f3a30b8b1e9b904210cf0a445ae9303d7d4c4d
adele <icestorm17@mail.ru>
2025-03-07 09:18:22 +0300

    Создан предварительный дизайн пользовательского окна, класс NotifyPropertyChanged, который будет отвечать за обработку событий, класс MainViewModel, который в будущем будет содержать основную модель поведения

---
+++ TestTask/NotifyPropertyChanged.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TestTask
+{
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ TestTask/Windows/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestTask.Windows;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ShowBalance_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Я баланс, я работаю");
+    }
+
+    private void GetTrades_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Я трейды, я работаю");
+    }
+
+    private void GetCandles_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Я свечи, я работаю");
+    }
+
+    private void GetTicker_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Я тикер, я работаю");
+    }
+
+    private void Update_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Я обновление, я работаю");
+    }
+
+}
